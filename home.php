a5720ec8f2f82a0af3d496e652366834ad4a3b9d
huyen1234567890 <huyenngoc3654238@gmail.com>
2024-08-16 20:24:57 +0700

    lab1

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 1</title>
+</head>
+<body>
+    <?php
+    echo '<h1 style="text-align: center; color: red;"> Xin Chào TMU </h1>';
+    echo '<button style="text-align: center; color: green;">Click</button>';
+    echo '<a target="_blank" href="https://www.youtube.com/">Youtube</a>';
+    ?>
+   
+</body>
+</html>
